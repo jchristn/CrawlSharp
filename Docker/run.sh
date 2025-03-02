41bb4463542e9ed9c598c8509c5a1920da3aa358
Joel Christner <joel.christner@gmail.com>
2025-03-02 12:46:44 -0800

    Docker changes

--- Docker/run.sh
+++ Docker/run.sh
@@ -1,29 +1,13 @@
 if [ -z "${IMG_TAG}" ]; then
-  IMG_TAG='v3.1.0'
+  IMG_TAG='v1.0.0'
 fi
 
 echo Using image tag $IMG_TAG
 
-if [ ! -f "litegraph.json" ]
-then
-  echo Configuration file litegraph.json not found.
-  exit
-fi
-
-# Items that require persistence
-#   litegraph.json
-#   litegraph.db
-#   logs/
-
-# Argument order matters!
-
 docker run \
-  -p 8701:8701 \
+  -p 8000:8000 \
   -t \
   -i \
   -e "TERM=xterm-256color" \
-  -v ./litegraph.json:/app/litegraph.json \
-  -v ./litegraph.db:/app/litegraph.db \
-  -v ./logs/:/app/logs/ \
-  jchristn/litegraph:$IMG_TAG
+  jchristn/crawlsharp:$IMG_TAG
 
